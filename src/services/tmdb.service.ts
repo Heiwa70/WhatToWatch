98ae565490bf61bd935ddcad2c8eb9e2f7a3cb6a
Sneegels <tweekgottfried@gmail.com>
2024-02-29 08:40:39 +0100

    Ajout de la fonction Genre pour les films

--- src/services/tmdb.service.ts
+++ src/services/tmdb.service.ts
@@ -3,26 +3,35 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from 'src/models/Movies';
 import { map } from 'rxjs/operators';
+import { Genre } from 'src/models/Genre';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
-  private url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  private url : string;
   private headers = new HttpHeaders({
     'Accept': 'application/json',
     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzEyYmY2YTBiZDg3OWRjMjc2ZTA0MTlmNmQxMzU0NSIsInN1YiI6IjY1ZDY0MDI1ZGIxNTRmMDE2NGEwOGMxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sEpl3yNo5-HSpmuN8mA05LTHSUkMaf-7TXMQlW94LZ4'
   });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.url = '';
+   }
 
   getPopularMovies(): Observable<Movie[]> {
+    this.url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
     return this.http.get<{results: Movie[]}>(this.url, { headers: this.headers })
     .pipe(
       map(response => response.results)
     );
   }
   
-
-
+  getGenreMovies(): Observable<Genre[]> {
+    this.url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+    return this.http.get<{genres: Genre[]}>(this.url, { headers: this.headers })
+    .pipe(
+      map(response => response.genres)
+    );
+  }
 }
